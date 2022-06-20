fadbb36310a3830b3afd102570d3af9fa24faca1
leon <gomesleon342@gmail.com>
2022-06-20 15:13:41 -0300

    added files

---
+++ precache-manifest.e536f70ee30fdccafa356a764132530c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b341d41745c4c847c93055ea222726eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef3f3d60e57b8056e6fd",
+    "url": "/static/css/main.8c5a84d8.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3f3d60e57b8056e6fd",
+    "url": "/static/js/main.c4762f03.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
